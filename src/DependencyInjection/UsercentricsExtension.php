91d10f21652f8fb15f9af27593d5ac0536838e2c
Andreas Kienast <andreas.kienast@typo3.com>
2026-04-28 15:10:45 +0200

    [TASK] Use correct `Extension` base class

--- src/DependencyInjection/UsercentricsExtension.php
+++ src/DependencyInjection/UsercentricsExtension.php
@@ -13,8 +13,8 @@ namespace T3G\Bundle\UsercentricsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class UsercentricsExtension extends Extension
 {
